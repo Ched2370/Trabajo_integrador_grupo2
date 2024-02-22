b976555f6ca192d868af25069825f98baf50b16f
Ched2370 <mmibz.85@gmail.com>
2024-02-22 09:20:22 -0300

    subiendo calculadora.html, formulario.html, juegoDeDados.html + style y logicas

---
+++ logica/juegoDeDados.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("tirar");
+btn.addEventListener("click", tirarDados);
+
+function tirarDados() {
+  try {
+    let vueltas = Math.floor(Math.random() * 30) + 15;
+    const dado = document.getElementById("dado");
+    console.log('cantidad de vueltas ' + vueltas);
+    for (let i = 0; i < vueltas; i++) {
+      const num = Math.floor(Math.random() * 6) + 1;
+      setTimeout(() => {
+        dado.src = "../imagenes/juego de dados/" + num + ".JPG";
+      }, i * 100);
+    }
+  } catch (err) {
+    swal("ERROR", "Ha surjido un error" + err.message, "error");
+  }
+}
