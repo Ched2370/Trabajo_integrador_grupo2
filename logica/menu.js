b6ed411a25eb2fbec7e75b849c9d930ac381afc1
Ched2370 <mmibz.85@gmail.com>
2024-02-23 14:43:20 -0300

    cambaindo script de menu por foreach y colocando fondo al :hover

--- logica/menu.js
+++ logica/menu.js
@@ -1,9 +1,24 @@
-const lista_menu = document.getElementsByClassName('lista-menu');
+const lista_menu = document.getElementsByClassName('lista-menu')[0];
 
-lista_menu[0].innerHTML = 
+/*lista_menu[0].innerHTML = 
 '<li><a href="Inicio.html">Inicio</a></li>' +
 '<li><a href="paginas/calculadora.html">Calculadora</a></li>' +
 '<li><a href="paginas/carrucel.html">Carrucel</a></li>' +
 '<li><a href="paginas/Fondos.html">Fondos</a></li>' +
 '<li><a href="paginas/juegoDeDados.html">Juego de dados</a></li>' +
-'<li><a href="paginas/formularioContacto.html">Contacto</a></li>';
+'<li><a href="paginas/formularioContacto.html">Contacto</a></li>';*/
+
+const lista = ['Inicio', 'Calculadora', 'Carrusel', 'Fondos', 'Juego de dados', 'Contacto'];
+const links = ['index.html', "paginas/calculadora.html", "paginas/carrusel.html", "paginas/Fondos.html", "paginas/juegoDeDados.html", "paginas/formularioContacto.html"];
+const li = document.createElement('li');
+
+links.forEach((link, i) => {
+    const a = document.createElement('a');
+    a.href = link;  
+    a.innerText = lista[i];  
+    li.appendChild(a)
+    lista_menu.appendChild(li);
+});
+
+console.log(lista_menu);
+
--- logica/menuPagSecundarias.js
+++ logica/menuPagSecundarias.js
@@ -1,9 +1,23 @@
-const lista_menu = document.getElementsByClassName('lista-menu');
+const lista_menu = document.getElementsByClassName('lista-menu')[0];
 
-lista_menu[0].innerHTML = 
+/*lista_menu[0].innerHTML = 
 '<li><a href="../index.html">Inicio</a></li>' +
 '<li><a href="calculadora.html">Calculadora</a></li>' +
 '<li><a href="Carrusel.html">Carrucel</a></li>' +
 '<li><a href="Fondos.html">Fondos</a></li>' +
 '<li><a href="juegoDeDados.html">Juego de dados</a></li>' +
-'<li><a href="formularioContacto.html">Contacto</a></li>';
+'<li><a href="formularioContacto.html">Contacto</a></li>';*/
+
+const lista = ['Inicio', 'Calculadora', 'Carrusel', 'Fondos', 'Juego de dados', 'Contacto'];
+const links = ['../index.html', "calculadora.html", "carrusel.html", "Fondos.html", "juegoDeDados.html", "formularioContacto.html"];
+const li = document.createElement('li');
+
+links.forEach((link, i) => {
+    const a = document.createElement('a');
+    a.href = link;  
+    a.innerText = lista[i];  
+    li.appendChild(a)
+    lista_menu.appendChild(li);
+});
+
+console.log(lista_menu);
