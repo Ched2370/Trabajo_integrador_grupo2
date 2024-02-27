f2e48d334eb55de8ed8ce3fad4d21e11c3a67c3a
Ched2370 <mmibz.85@gmail.com>
2024-02-27 19:05:39 -0300

    pagina formulario terminada

--- logica/formulario.js
+++ logica/formulario.js
@@ -6,28 +6,72 @@ const tbody = document.getElementById("tbody");
 var contador = 0;
 function validar() {
   try {
-    const apellido = document.getElementById("apellido").value;
-    const nombre = document.getElementById("nombre").value;
-    const email = document.getElementById("email").value;
-    const textarea = document.getElementById("textarea").value;
+    const eliminar = document.querySelectorAll('.advertencia');
+      eliminar.forEach((eliminar) => {
+        eliminar.remove();
+      });
+
+    const apellido = document.getElementById("apellido");
+    const nombre = document.getElementById("nombre");
+    const email = document.getElementById("email");
+    const textarea = document.getElementById("textarea");
     const patron = /^[a-zA-ZÁÉÍÓÚáéíóúÑñ\s']+$/;
     const patronEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const patronConsulta = /^[a-zA-Z0-9\s.-_]+$/;
-    var datos = [apellido, nombre, email, textarea];
+    const patronConsulta = /^[a-zA-Z0-9.-_?\s]+$/;
+    const datos = [apellido.value, nombre.value, email.value, textarea.value];
+      console.log(datos);
+      console.log(patron.test(apellido.value));
+      console.log(patron.test(nombre.value));
+      console.log(patronEmail.test(email.value));
+      console.log(patronConsulta.test(textarea.value));
 
     if (
-      apellido &&
-      nombre &&
-      email &&
-      textarea &&
-      patron.test(apellido) &&
-      patron.test(nombre) &&
-      patronEmail.test(email) &&
-      patronConsulta.test(textarea)
+      apellido.value &&
+      nombre.value &&
+      email.value &&
+      textarea.value &&
+      patron.test(apellido.value) &&
+      patron.test(nombre.value) &&
+      patronEmail.test(email.value) &&
+      patronConsulta.test(textarea.value)
     ) {
       agregar(datos);
+      console.log('paso');
     } else {
-      console.log("No paso");
+      let padre = document.querySelectorAll(".lbl");
+      if (!apellido.value || !patron.test(apellido.value)) {
+        let ape = document.createElement("p");
+        ape.className = "advertencia";
+        ape.innerHTML = "agrege su apellido*";
+        ape.style.color = "crimson";
+        padre[0].appendChild(ape);
+        apellido.insertAdjacentElement("afterend", ape);
+      }
+      if (!nombre.value || !patron.test(nombre.value)) {
+        let nom = document.createElement("p");
+        nom.className = "advertencia";
+        nom.innerHTML = "agrege su nombre*";
+        nom.style.color = "crimson";
+        padre[1].appendChild(nom);
+        nombre.insertAdjacentElement("afterend", nom);
+      }
+      if (!email.value || !patronEmail.test(email.value)) {
+        let mail = document.createElement("p");
+        mail.className = "advertencia";
+        mail.innerHTML = "agrege su e-mail*";
+        mail.style.color = "crimson";
+        padre[2].appendChild(mail);
+        email.insertAdjacentElement("afterend", mail);
+      }
+      if (!textarea.value || patronConsulta.test(textarea.value)) {
+        let text = document.createElement("p");
+        text.className = "advertencia";
+        text.innerHTML = "agrege su consulta*";
+        text.style.color = "crimson";
+        padre[3].appendChild(text);
+        textarea.insertAdjacentElement("afterend", text);
+      }
+      console.log("no paso");
     }
   } catch (err) {
     alert("¡Ha ocurrido un error!" + err.message);
@@ -35,7 +79,7 @@ function validar() {
 }
 
 function agregar(d) {
-  try { 
+  try {
     if (contador === 0) {
       let cabeceraDatos = ["Apellido", "Nombre", "Email", "Mensaje"];
       let fila = document.createElement("tr");
@@ -58,9 +102,11 @@ function agregar(d) {
       columna.innerText = d[i];
       fila.appendChild(columna);
     }
-    apellido.value = '';
-    nombre.value = '';
-    email.value = '';
-    textarea.value = '';
-  } catch (error) {}
+    apellido.value = "";
+    nombre.value = "";
+    email.value = "";
+    textarea.value = "";
+  } catch (err) {
+    alert('Ha surjido  un error');
+  }
 }
